502b2d2fc42fe4c7250b5e2d7db9278193983b1c
Duve, Michael <michael.duve@bild.de>
2015-04-09 12:56:34 +0200

    ex1: a1 finished

---
+++ ex1/a1/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * MME exercise 1
+ * author: Michael Duve
+ * matnr: 798419
+ */
+
+function displayConsoleMessage() {
+    "use strict";
+    console.log("app is started");
+}
+
+window.onload = function() {
+    "use strict";
+
+    displayConsoleMessage();
+
+};
