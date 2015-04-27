18fe2e873cae3c078a9839c63266b13555360e6a
Duve, Michael <michael.duve@bild.de>
2015-04-27 17:08:28 +0200

    ex3: a3 copy and uglify working

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,30 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-            build: {
-                src: 'src/server.js',
-                dest: 'built/server.min.js'
+            ex3a3: {
+                files: [{
+                    src: 'ex3/a3/src/server.js',
+                    dest: 'ex3/a3/built/server.js'
+                }]
+            }
+        },
+        copy: {
+            ex3a3: {
+                expand: true,
+                cwd: 'ex3/a3/src/public',
+                src: ['**'],
+                dest: 'ex3/a3/built/public'
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify:ex3a3', 'copy:ex3a3']);
 
 };
