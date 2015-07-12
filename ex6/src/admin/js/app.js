8942376206662cb3d466cb7973404c048d827b22
Duve, Michael <michael.duve@bild.de>
2015-07-12 21:55:50 +0200

    angularJS used: view all and updated book working

--- ex6/src/admin/js/app.js
+++ ex6/src/admin/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*global $*/
+/*global $,angular*/
 
 /**
  * MME exercise 6
@@ -8,118 +8,70 @@
  * matnr: 798419
  */
 
-var pathToApi = "/api/books";
+angular.module('libraryApp', [])
 
-$(document).ready(function() {
+    .controller('BookController', function($scope, $http, $sce) {
 
-    var $body = $("body");
+        $scope.getAll = function() {
+            $http.get('http://localhost:1337/api/books/').then(function(resp) {
+                console.log($scope.editBook);
+                $scope.books = resp.data;
+            }, function(err) {
+                errorHandler(err);
+            });
+        };
 
-    var bookSource   = $("#book-list-template").html();
-    var bookListTemplate = Handlebars.compile(bookSource);
+        $scope.deleteBook = function(id) {
+            $http.delete('http://localhost:1337/api/books/'+id).then(function(resp) {
+                console.log("deleted " + id);
+                $scope.getAll();
+            }, function(err) {
+                errorHandler(err);
+            });
+        };
 
-    var notificationSource   = $("#notification-template").html();
-    var notificationTemplate = Handlebars.compile(notificationSource);
+        $scope.editBook = function(id) {
+            $http.get('http://localhost:1337/api/books/'+id).then(function(resp) {
+                $scope.editBookData = resp.data;
+                $(".collapse.in").collapse('toggle');
+                $('#collapseEdit').collapse('toggle');
+            }, function(err) {
+                errorHandler(err);
+            });
+        };
 
-    var editSource   = $("#book-form-template").html();
-    var editTemplate = Handlebars.compile(editSource);
+        $scope.updateBook = function(item) {
 
-    scroll_if_anchor(window.location.hash);
-    $body.on("click", "a[href^='#']", scroll_if_anchor);
+            var authors = [];
 
-    $(".navbar-nav").on("click", "li a", function() {
-        var $listItem = $(this).parent();
-
-        if ($listItem.hasClass("active")) {
-            return false;
-        }
-
-        $(".navbar-nav .active").removeClass("active");
-        $listItem.addClass("active");
-
-    });
-
-    $body.on("click", ".delete", function() {
-        var id = $(this).data("delete-book");
-        deleteBook(id, function(data) {
-            $(".notification-container").html(notificationTemplate(data));
-        });
-    });
-
-    $body.on("click", ".edit", function() {
-        var id = $(this).data("edit-book");
-        editBook(id, function(data) {
-            console.log(data);
-        });
-    });
-
-    $("#addbook").submit(function(e) {
-        var data = {};
-        $(this).serializeArray().map(function(x){
-            if (x.value && x.value !== null && x.value !== "") {
-                data[x.name] = x.value;
+            for (var i in item.author) {
+                if (item.author.hasOwnProperty(i)) {
+                    authors.push(item.author[i].name);
+                }
             }
-        });
-        if (data.state) {
-            data.state = 1;
-        }
-        if (data.author) {
-            data.author = data.author.split(",");
-        }
-        console.log(data);
-        e.preventDefault();
-        return false;
+
+            item.author = authors;
+
+            $http.put('http://localhost:1337/api/books/'+item._id, item).then(function(resp) {
+                $(".collapse.in").collapse('toggle');
+                $('#collapseList').collapse('toggle');
+            }, function(err) {
+                errorHandler(err);
+            });
+
+        };
+
+        // HELPER
+        $scope.showState = function(input) {
+            return input === 1 ? 'online' : 'offline';
+        };
+
+        $scope.showChecked = function(input) {
+            return input === 1 ? 'checked' : '';
+        };
+
     });
 
-    getAllBooks(function(books) {
-        $(".book-list").html(bookListTemplate(books));
-    });
-});
-
-function getAllBooks(_callback) {
-    $.ajax({
-        method: "GET",
-        url: pathToApi,
-        dataType: "json"
-    }).success(function(data) {
-        _callback(data);
-    });
-}
-
-function deleteBook(id, _callback) {
-    $.ajax({
-        method: "DELETE",
-        url: pathToApi + "/" + id,
-        dataType: "json"
-    }).success(function() {
-        _callback(id);
-    });
-}
-
-function editBook(id, _callback) {
-    $.ajax({
-        method: "GET",
-        url: pathToApi + "/" + id,
-        dataType: "json"
-    }).success(function() {
-        _callback(id);
-    });
-}
-
-function scroll_if_anchor(href) {
-    href = typeof(href) === "string" ? href : $(this).attr("href");
-    if (!href || $(this).attr("data-parent") === "#accordion") {
-        return;
-    }
-    var fromTop = 70,
-        $target = $(href);
-
-    // Older browsers without pushState might flicker here, as they momentarily
-    // jump to the wrong position (IE < 10)
-    if($target.length) {
-        $('html, body').animate({ scrollTop: $target.offset().top - fromTop });
-        if(history && "pushState" in history) {
-            history.pushState({}, document.title, window.location.pathname + href);
-            return false;
-        }
-    }
+function errorHandler(err) {
+    console.error(err);
 }
